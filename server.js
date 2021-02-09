31220336aab04f056e9f0b3e0408f78844a19bf5
mapatag <64618397+mapatag@users.noreply.github.com>
2021-02-08 19:33:38 -0500

    Update server.js

--- server.js
+++ server.js
@@ -127,12 +127,12 @@ function serverHandler(request, response) {
         try {
             stats = fs.lstatSync(filename);
 
-            if (filename.search(/demos/g) === -1 && filename.search(/admin/g) === -1 && stats.isDirectory() && config.homePage === '/demos/dashboard/index.html') {
+            if (filename.search(/demos/g) === -1 && filename.search(/admin/g) === -1 && stats.isDirectory() && config.homePage === '/demos/index.html') {
                 if (response.redirect) {
-                    response.redirect('/demos/dashboard');
+                    response.redirect('/demos/');
                 } else {
                     response.writeHead(301, {
-                        'Location': '/demos/dashboard'
+                        'Location': '/demos/'
                     });
                 }
                 response.end();
